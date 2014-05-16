713fe6004bdf9e734e23368e8fa1037be7a4adbd
Chen KangMing <chenkm@ucweb.com>
2014-05-16 18:24:54 +0800

    新增字段

--- src/com/x/app/common/UIHelper.java
+++ src/com/x/app/common/UIHelper.java
@@ -11,6 +11,18 @@ package com.x.app.common;
  */
 public class UIHelper {
 	public final static int LISTVIEW_ACTION_INIT = 0x01;
-	public final static int LISTVIEW_ACTION_REFRESH=0x02;
-	public final static int LISTVIEW_SCROLL=0x03;
+	public final static int LISTVIEW_ACTION_REFRESH = 0x02;
+	public final static int LISTVIEW_ACTION_SCROLL = 0x03;
+	public final static int LISTVIEW_ACTION_CHANGE_CATALOG = 0x04;
+
+	public final static int LISTVIEW_DATA_MORE = 0x01;
+	public final static int LISTVIEW_DATA_LOADING = 0x02;
+	public final static int LISTVIEW_DATA_FULL=0x03;
+	public final static int LISTVIEW_DATA_EMPTY=0x04;
+	
+	public final static int LISTVIEW_DATATYPE_NEWS=0x01;
+	public final static int LISTVIEW_DATATYPE_BLOG=0x02;
+	public final static int LISTVIEW_DATATYPE_POST=0x03;
+	public final static int LISTVIEW_DATATYPE_TWEET=0x04;
+
 }
